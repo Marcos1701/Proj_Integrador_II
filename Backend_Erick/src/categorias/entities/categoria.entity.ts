3024a038f2f1a40a515f4afa954351b27122586d
erick7amorim <erickamorimcarv@gmail.com>
2023-10-29 16:43:06 -0300

    model de categoria criados

--- Backend_Erick/src/categorias/entities/categoria.entity.ts
+++ Backend_Erick/src/categorias/entities/categoria.entity.ts
@@ -1,1 +1,17 @@
-export class Categoria {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  descricao: string;
+
+  constructor(categoria: Partial<Categoria>) {
+    Object.assign(this, categoria);
+  }
+}
