71731603ba0258bc1eae7816f9eb84786f8b7b42
erick7amorim <erickamorimcarv@gmail.com>
2023-10-30 13:23:52 -0300

    entidade e controller de usuário criado

--- Backend_Erick/src/app.module.ts
+++ Backend_Erick/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { CategoriasModule } from './categorias/categorias.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     CategoriasModule,
     DatabaseModule,
+    UsuariosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
